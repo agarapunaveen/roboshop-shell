1195c7cda058c6241aec161fe3210d5687d2a292
naveen <agarapunaveen@gmail.com>
2024-07-24 20:10:48 +0530

    shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,4 +15,14 @@ do
     --resources $instance_id \
     --tags Key=name,Value=$name
     echo "instance created for:$name"
+   if [ $name == "web" ]
+      then
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        ip_to_use=$public_ip
+      else
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        ip_to_use=$private_ip
+
+    fi
+    echo "instance name:$name ip_address:$ip_to_use
 done
